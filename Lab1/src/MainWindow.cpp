79aaf9d3a092541326410612ba3d4e76be8cfa7f
Kirill <ktrapsha@gmail.com>
2024-11-26 16:32:47 +0300

    aboba

--- Lab1/src/MainWindow.cpp
+++ Lab1/src/MainWindow.cpp
@@ -7,14 +7,12 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       centralWidget(new QWidget(this)),
       mainLayout(new QVBoxLayout),
-      title1(new QLabel("Контроль и управление потенциометром через терминал по интерфейсу USB-RS485", this)),
-      title2(new QLabel("\tЛабораторная работа 1", this)),
+      title1(new QLabel("\tЛабораторная работа 1", this)),
       tabWidget(new QTabWidget(this)) {
 
     // Настройка заголовков
     title1->setStyleSheet("font-size: 24px; font-weight: bold; text-align: center;");
-    title2->setStyleSheet("font-size: 18px; text-align: center;");
-
+    
     // Добавление вкладок через отдельные классы
     tabWidget->addTab(new Tab1Widget(this), "1");
     tabWidget->addTab(new Tab2Widget(this), "2");
@@ -23,8 +21,6 @@ MainWindow::MainWindow(QWidget *parent)
     // Компоновка
     mainLayout->addWidget(title1);
     title1->setAlignment(Qt::AlignLeft);
-    mainLayout->addWidget(title2);
-    title2->setAlignment(Qt::AlignLeft);
     mainLayout->addWidget(tabWidget);
 
     centralWidget->setLayout(mainLayout);
